31e7c4eae47cae3682810c565df0d6f6c3589ade
MaJ-pl <m.jaszczynski@icloud.com>
2023-12-27 09:42:27 +0100

    rev

---
+++ app/Livewire/SearchBox.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SearchBox extends Component
+{
+    public  $search;
+
+    public function updatedSearch()
+    {
+    }
+
+    public function render()
+    {
+        return view('livewire.search-box');
+    }
+}
